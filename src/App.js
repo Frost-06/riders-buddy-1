f4f299d7666d5e61e6152a1e5ad4f2ac9ee3f283
Fros-06 <andredulaguinita@gmail.com>
2022-01-20 12:14:35 +0800

    init Sign up

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Product from "./routes/Product";
 import Dashboard from "./routes/Dashboard";
 import SignIn from "./routes/SignIn";
+import SignUp from "./routes/SignUp"
 
 function App() {
   return (
     <Routes>
       <Route path="/product" element={<Product />} />
       <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
       <Route path="/" element={<Dashboard />} />
     </Routes>
   );
