15ab1e545f17b92f22366f6a1f464053556d39e6
azkalonz <aeartixenter@gmail.com>
2022-01-20 21:42:58 +0800

    fix chat

--- src/components/Chat/ChatContainer.js
+++ src/components/Chat/ChatContainer.js
@@ -1,12 +1,58 @@
-import React from "react";
+import {
+  Box,
+  Button,
+  Container,
+  List,
+  ListItem,
+  ListItemText,
+  SwipeableDrawer,
+  Typography,
+} from "@mui/material";
+import React, { useEffect } from "react";
 import Chat from "../Chat/Chat";
 
 function ChatContainer(props) {
-  return (
-    <div className="chat-container">
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event &&
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 464 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
       <Chat />
-      <div className="chat-overlay"></div>
-    </div>
+    </Box>
+  );
+  useEffect(() => {
+    setState({ ...state, right: true });
+  }, []);
+  return (
+    <SwipeableDrawer
+      anchor={"right"}
+      open={state["right"]}
+      onClose={toggleDrawer("right", false)}
+      onOpen={toggleDrawer("right", true)}
+    >
+      {list("right")}
+    </SwipeableDrawer>
   );
 }
 
