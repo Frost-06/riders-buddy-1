9b3b073aa6574d04aa514ce18dc9c4a2beaf0a93
Fros-06 <andredulaguinita@gmail.com>
2022-01-20 18:44:32 +0800

    updating router link

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { Route, Routes } from "react-router-dom";
-import Product from "./routes/Product";
+import ProductPage from "./routes/ProductPage";
 import Dashboard from "./routes/Dashboard";
 import SignIn from "./routes/SignIn";
 import SignUp from "./routes/SignUp"
+import ServicePage from "./routes/ServicePage"
 
 function App() {
   return (
     <Routes>
-      <Route path="/product" element={<Product />} />
+      <Route path="/product" element={<ProductPage />} />
       <Route path="/sign-in" element={<SignIn />} />
       <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/service" element={<ServicePage />} />
       <Route path="/" element={<Dashboard />} />
     </Routes>
   );
