ee7eec3e8b3c2d04da052926eeb5576b4acd4d69
Fros-06 <andredulaguinita@gmail.com>
2022-01-21 20:08:49 +0800

    customer review

--- src/components/Product/TabPage.js
+++ src/components/Product/TabPage.js
@@ -4,6 +4,8 @@ import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import LinearProgress from "@mui/material/LinearProgress";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -60,41 +62,108 @@ export default function TabPage() {
         </Tabs>
       </Box>
       <TabPanel value={value} index={0}>
-        <Typography variant="h1" style={{fontWeight: "bold", marginBottom: "32px"}}>Product Details</Typography>
+        <Typography
+          variant="h1"
+          style={{ fontWeight: "bold", marginBottom: "32px" }}
+        >
+          Product Details
+        </Typography>
         <Typography variant="label">
-        Easy to install chain joints for the magic buckle easy to disassemble easy to clean reusable 
-            Strong Rally special forging steel repeated forging strict control of materials to ensure the strength
-            Smooth lubrication surface polishing internal oiling
-            Accurate matching professional equipment test spacing professional and technical test data  
-            Durable rust and corrosion
+          Easy to install chain joints for the magic buckle easy to disassemble
+          easy to clean reusable Strong Rally special forging steel repeated
+          forging strict control of materials to ensure the strength Smooth
+          lubrication surface polishing internal oiling Accurate matching
+          professional equipment test spacing professional and technical test
+          data Durable rust and corrosion
         </Typography>
 
-        <Typography variant="h1" style={{fontWeight: "bold", marginBottom: "32px", marginTop: "32px"}}>Specifications</Typography>
-        <Box style={{display: "flex", flexDirection: "column", gap: "24px"}}>
-        <Typography variant="label" style={{display: "inline-flex", alignItems: "center", gap: "22px"}}>
+        <Typography
+          variant="h1"
+          style={{
+            fontWeight: "bold",
+            marginBottom: "32px",
+            marginTop: "32px",
+          }}
+        >
+          Specifications
+        </Typography>
+        <Box style={{ display: "flex", flexDirection: "column", gap: "24px" }}>
+          <Typography
+            variant="label"
+            style={{
+              display: "inline-flex",
+              alignItems: "center",
+              gap: "22px",
+            }}
+          >
             <img src="/img/checkbox.png" />
             100% brand new and high quality
-        </Typography>
-        <Typography variant="label" style={{display: "inline-flex", alignItems: "center", gap: "22px"}}>
+          </Typography>
+          <Typography
+            variant="label"
+            style={{
+              display: "inline-flex",
+              alignItems: "center",
+              gap: "22px",
+            }}
+          >
             <img src="/img/checkbox.png" />
             Made of high-quality materials, durable and practical
-        </Typography>
-        <Typography variant="label" style={{display: "inline-flex", alignItems: "center", gap: "22px"}}>
+          </Typography>
+          <Typography
+            variant="label"
+            style={{
+              display: "inline-flex",
+              alignItems: "center",
+              gap: "22px",
+            }}
+          >
             <img src="/img/checkbox.png" />
             Type: Single speed positive and negative plate (circular disc)
-        </Typography>
-        <Typography variant="label" style={{display: "inline-flex", alignItems: "center", gap: "22px"}}>
+          </Typography>
+          <Typography
+            variant="label"
+            style={{
+              display: "inline-flex",
+              alignItems: "center",
+              gap: "22px",
+            }}
+          >
             <img src="/img/checkbox.png" />
             Material: 7075-T6 Aviation Aluminum Alloy
-        </Typography>
-        <Typography variant="label" style={{display: "inline-flex", alignItems: "center", gap: "22px"}}>
+          </Typography>
+          <Typography
+            variant="label"
+            style={{
+              display: "inline-flex",
+              alignItems: "center",
+              gap: "22px",
+            }}
+          >
             <img src="/img/checkbox.png" />
             Weight: 34T: 4kg (Approx)
-        </Typography>
+          </Typography>
         </Box>
       </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item Two
+      <TabPanel value={value} index={1} style={{ display: "inline-flex" }}>
+        <Container
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography
+            variant="h1"
+            style={{ color: "#1AA3E9", fontWeight: "bold" }}
+          >
+            5
+          </Typography>
+          <Typography>1k customer reviews</Typography>
+        </Container>
+        <Container style={{ width: "100%" }}>
+          <LinearProgress variant="determinate" value={20} />
+        </Container>
       </TabPanel>
       <TabPanel value={value} index={2}>
         Item Three
